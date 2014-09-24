4d7ef42b5c9f13a8a1f7dd2f922ee3a531f05adf
mheki <mpietrzak@inviqa.com>
2014-09-24 16:01:50 +0100

    stream filters

--- socket_client.php
+++ socket_client.php
@@ -1,5 +1,6 @@
 <?php
 $socket = stream_socket_client('tcp://127.0.0.1:888');
+stream_filter_append($socket, 'zlib.inflate');
 
 while (!feof($socket)) {
     echo fread($socket, 100);
--- socket_server.php
+++ socket_server.php
@@ -2,7 +2,11 @@
 $socket = stream_socket_server('tcp://127.0.0.1:888');
 
 while ($conn = stream_socket_accept($socket)) {
-    print '--- Client connected to the server ---' . "\n";
+    fwrite(STDOUT, '--- Client connected to the server ---' . "\n");
+
+    stream_filter_append($conn, 'string.toupper');
+    stream_filter_append($conn, 'zlib.deflate');
+
     fwrite($conn, "Hello world\n");
     fclose($conn);
 }
